7b2eda0689a2e4db7c8f9db2c876adf4d0a7a4ef
Harish20-01 <harish.venkat0120@gmail.com>
2025-05-21 10:33:59 +0530

    Review Url chnaged in delete component

--- client/src/updateComponents/ReviewDeleteComponent.jsx
+++ client/src/updateComponents/ReviewDeleteComponent.jsx
@@ -17,7 +17,7 @@ const ReviewDeleteComponent = () => {
   useEffect(() => {
     const fetchReviews = async () => {
       try {
-        const response = await axios.get("${url}/reviews");
+        const response = await axios.get(`${url}/reviews`);
         setReviews(response.data);
       } catch (error) {
         console.error('Error fetching reviews:', error);
